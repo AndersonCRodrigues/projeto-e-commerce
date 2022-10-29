7379734551b1782d54fb3fd040eec2a098d8361f
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-10-29 19:12:23 -0300

    adiciona logo ao header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,13 @@
 import '../../style/header.css';
 import { CarrinhoIcon } from '../carrinhoIcon';
+import { Logo } from '../logo';
 import { RouteLinks } from '../navLinks';
 
 export const Header = () => {
   return (
     <header>
       <h1>aqui fica o cabeçalho</h1>
+      <Logo />
       <RouteLinks />
       <CarrinhoIcon />
     </header>
---
+++ src/components/logo/index.jsx
@@ -0,0 +1,9 @@
+import '../../style/logo.css'
+
+export const Logo = () => {
+  return (
+    <div className='logo'>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/b/be/Nuvemshop-logo.png" alt="nuvemshop" />
+    </div>
+  );
+}
